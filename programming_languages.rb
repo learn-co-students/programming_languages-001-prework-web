b1bb264a1e5f5703ab5869e49d20af5130650f3b
hwinn4 <hilary.winn@gmail.com>
2015-09-14 15:34:31 -0400

    All elements of the hash are reformatted properly

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
 def reformat_languages(languages)
   # your code here
+  new_hash={}
+  languages.each do |style,langs|
+    langs.each do |name, type|
+      if new_hash.keys.include?(name) == false
+        new_hash[name] = type
+        new_hash[name].store(:style, [style])
+      else 
+        new_hash[name][:style] << style
+      end
+    end
+  end
+  new_hash
 end
