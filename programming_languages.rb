e142935849422d0008c5f218849216044b44b680
mbukantz <mbukantz@gmail.com>
2015-09-21 10:59:32 -0400

    done with this lab

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,54 @@
+require 'pry'
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  result = {}
+  languages.each do |style, values|
+    values.each do |language, attributes|
+      if result[language].nil?
+        result[language] = {}
+      end
+      attributes.each do |property, property_value|
+        if property == :type
+          result[language][:type] = property_value
+        end
+        if result[language][:style].nil?
+          result[language][:style] = []
+        end
+        result[language][:style] << style 
+      end
+    end
+  end
+  result
 end
