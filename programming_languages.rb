18600403b80e900e9ed2d1a033bfc1e5f9c75aec
mgsterling11 <m.g.sterling11@gmail.com>
2015-09-23 22:46:02 -0400

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
-def reformat_languages(languages)
-  # your code here
+
+
+def reformat_languages(languages_by_style)
+  sorted_languages = {}
+
+  languages_by_style.each do |style, languages_by_type|
+    languages_by_type.each do |language, info|
+      sorted_languages[language] ||= {}
+      sorted_languages[language][:type] = info[:type]
+      sorted_languages[language][:style] ||= []
+      sorted_languages[language][:style].push(style)
+    end
+  end
+    sorted_languages
 end
