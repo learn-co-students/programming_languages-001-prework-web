6068b96888b04ba927117d70c942f1d74f5ae7f0
eenymini <hhl262@nyu.edu>
2015-09-24 17:06:11 -0400

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, langs|
+    langs.each do |lang, type|
+      type.each do |x, y|
+        if new_hash.length < 7
+          new_hash[lang] = {x => y, :style => [style]}
+        else
+          new_hash[:javascript][:style] << style
+        end
+      end
+    end
+  end
+  new_hash
 end
