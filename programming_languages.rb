470c7e04181e715cd3dd48f7bc17c7db90f08480
wifelette <leahsilber@gmail.com>
2015-10-16 21:17:42 -0700

    Passing

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |oo_or_functional, list_of_langs|
+    list_of_langs.each do |language, details|
+      new_hash[language] = new_hash[language] || {
+        type: details[:type],
+        style: []
+      }
+      new_hash[language][:style] << oo_or_functional
+    end
+  end
+  new_hash
 end
