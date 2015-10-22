3c0d21ed6ee8c52a23fc6c69c62d72f76a8a7cd1
mikeappell <mikeappell@gmail.com>
2015-10-22 17:14:41 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
-  # your code here
+  languages_by_name = {}
+  languages.each do |style, data|
+    data.each do |name, type|
+      languages_by_name[name] = {} if languages_by_name[name].nil?
+      languages_by_name[name][:type] = type[:type]
+      languages_by_name[name][:style] = languages_by_name[name][:style].nil? ? [style] : languages_by_name[name][:style] + [style]
+    end
+  end
+  languages_by_name
 end
