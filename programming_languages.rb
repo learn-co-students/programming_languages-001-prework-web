fd7a8e9f1513b18301f2bdeca23a252da3d2e536
skipsuva <skip.suva@gmail.com>
2015-10-25 22:24:57 -0400

    Reorganized languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
-def reformat_languages(languages)
+#hash structure:
+#  key (style), nested_hesh(lang)
+#                    Key=lang, value=attribute
+#                          key = type, value= "data"  
+require 'pry'
+
+def reformat_languages(languages) 
   # your code here
+  new_hash = Hash.new
+  languages.each do |style, language_data_hash|
+    language_data_hash.each do |lang, attribute_hash|
+      attribute_hash.each do |type, data|
+        new_hash[lang] = {type: data}
+        #binding.pry
+      end
+    end
+  end
+  languages.each do |style, language_data_hash|
+    language_data_hash.each_key do |lang|
+      if new_hash[lang][:style]
+        new_hash[lang][:style] << style
+      else new_hash[lang][:style] = [style]
+      end
+    end
+  end
+  #new_hash[:javascript][:style] = [:oo, :functional]
+  new_hash
 end
