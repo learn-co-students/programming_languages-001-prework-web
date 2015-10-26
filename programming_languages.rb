b2ab2829dc4abb376ac19c4bfc6aa0b6d3494e37
flats <dave@robador.com>
2015-10-26 02:27:19 -0400

    first commit - reorganize languages

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, langs|
+    current_style = style
+    langs.each do |lang, type_h|
+      if new_hash.has_key?(lang) == false
+        new_hash[lang] = {
+          :type => type_h[:type],
+          :style => [current_style]
+        }
+      else
+        new_hash[lang][:style] << current_style
+      end
+    end
+  end
+  new_hash
 end
