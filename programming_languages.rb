1048f3bc33d3499b08e0f3d25abc81070490f576
uncledoofy156 <uncledoofy156@aim.com>
2015-11-08 20:15:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style, language|
+    new_hash = new_hash.merge(language)
+  end
+
+#   new_hash = languages.values.inject {|hash, elem| hash.merge(elem)}
+
+  new_hash.each do |language_n, attr|
+    attr[:style] = []
+    languages.each do |style, language|
+      attr[:style] << style if language.has_key?(language_n)
+    end
+  end
 end
