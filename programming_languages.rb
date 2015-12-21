b35edab79b9951889aa2844a1a18572e5982cdd7
Nathaniel Miller <dukeoflaser@gmail.com>
2015-12-21 00:25:34 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require 'pry'
+require 'pry-nav'
+
 def reformat_languages(languages)
-  # your code here
+  
+  reformatted = {}
+  languages.each do |style, language|
+    language.each do |lang, type|
+      if reformatted[lang] == nil
+        type[:style] = []
+        type[:style] << style
+        reformatted[lang] = type
+      else
+        reformatted[lang][:style] << style
+      end
+    end
+  end
+  
+  reformatted
+
 end
