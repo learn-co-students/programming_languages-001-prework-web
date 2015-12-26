f1f2a972ac92a349c595706bda17bb23b31952c0
chintas1 <chintas1@tcnj.edu>
2015-12-26 15:14:05 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  formatted = {}
+  languages.each do |type, langs|
+    langs.each do |lang, data|
+      formatted[lang] = data if (!(formatted.has_key?(lang)))
+      if(formatted[lang].keys.size < 2)
+        formatted[lang][:style] = [type]
+      else
+        formatted[lang][:style] << type
+      end
+    end
+  end
+  formatted
 end
