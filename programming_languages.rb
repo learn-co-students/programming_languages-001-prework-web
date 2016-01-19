b3a3aa5a56e79995d02dfe85685c17f3a9a3aa20
fossbender <fossbender@gmail.com>
2016-01-19 03:04:14 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
-def reformat_languages(languages)
-  # your code here
+require 'pry'
+
+def reformat_languages(lang)
+  new_hsh = {}
+  lang.each do | prog_style, value |
+    value.each do | language, type |
+      if !new_hsh.keys.include? (language)
+        new_hsh[language] = { type: value[language][:type], style: [prog_style]}
+      else
+        new_hsh[language][:style] << prog_style
+      end
+    end
+  end
+  puts new_hsh
+  new_hsh
 end
