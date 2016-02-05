b359ef0cfd33b2648fd6be7fde988ced4ce4b0fb
chintas1 <chintas1@tcnj.edu>
2016-02-05 09:40:27 -0500

    Refactors solution

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,22 @@
+# def reformat_languages(languages)
+#   formatted = {}
+#   languages.each do |type, langs|
+#     construct_lang_hashs(langs, type, formatted)
+#   end
+#   formatted
+# end
+
 def reformat_languages(languages)
-  # your code here
-  formatted = {}
-  languages.each do |type, langs|
-    langs.each do |lang, data|
-      formatted[lang] = data if (!(formatted.has_key?(lang)))
-      if(formatted[lang].keys.size < 2)
-        formatted[lang][:style] = [type]
-      else
-        formatted[lang][:style] << type
-      end
-    end
+  languages.each_with_object({}) do |(type, langs), formatted|
+    construct_lang_hashs(langs, type, formatted)
   end
-  formatted
 end
+
+def construct_lang_hashs(langs, lang_type, new_hash)
+  langs.each do |lang, lang_data|
+    new_hash[lang] = lang_data if (!(new_hash.has_key?(lang)))
+    new_hash[lang].keys.size < 2 ? new_hash[lang][:style] = [lang_type] : new_hash[lang][:style] << lang_type
+  end
+end
+
+
