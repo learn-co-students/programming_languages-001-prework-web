51074be504f819be3e1010198861e71873ce42aa
fossbender <fossbender@gmail.com>
2016-02-06 11:00:11 +0000

    nitrous-backup at Sat Feb  6 11:00:11 UTC 2016

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,16 +1,68 @@
 require 'pry'
 
-def reformat_languages(lang)
-  new_hsh = {}
-  lang.each do | prog_style, value |
-    value.each do | language, type |
-      if !new_hsh.keys.include? (language)
-        new_hsh[language] = { type: value[language][:type], style: [prog_style]}
-      else
-        new_hsh[language][:style] << prog_style
-      end
+
+lang =
+{:oo => {
+    :ruby => {
+        :type => "interpreted"
+    },
+    :javascript => {
+        :type => "interpreted"
+    },
+    :python => {
+        :type => "interpreted"
+    },
+    :java => {
+        :type => "compiled"
+    }
+},
+    :functional => {
+    :clojure => {
+        :type => "compiled"
+    },
+    :erlang => {
+        :type => "compiled"
+    },
+    :scala => {
+        :type => "compiled"
+    },
+    :javascript => {
+        :type => "interpreted"
+    }
+}
+}
+
+#testing for guy on learn
+def reformat_languages(languages)
+  new_hash = {}
+  arraystyles = []
+  languages.each do |styles, all_info|
+
+    all_info.each do   |programming_language, info|
+      puts info
+      new_hash[programming_language] = info
+      new_hash[programming_language][:style] = [styles]
     end
   end
-  puts new_hsh
-  new_hsh
+  puts new_hash
 end
+reformat_languages(lang)
+# end testing for guy on learn
+
+
+
+#def reformat_languages(lang)
+#  new_hsh = {}
+#  lang.each do | prog_style, value |
+#    value.each do | language, type |
+#      puts type
+#      if !new_hsh.keys.include? (language)
+#        new_hsh[language] = { type: value[language][:type], style: [prog_style]}
+#      else
+#        new_hsh[language][:style] << prog_style
+#      end
+#    end
+#  end
+#  puts new_hsh
+#  new_hsh
+#end
