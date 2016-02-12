ea298724a094fc12a3e49fc555cbe70fb0901473
Matt Cassara <cassaram09@gmail.com>
2016-02-11 23:39:59 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+  languages.each do |style, language|
+    language.each do |language2, type2|
+      type2.each do |type3, value| 
+        if hash.has_key?(language2) #if there is a key(language) that's already in the hash, add a value, do not overwrite
+          hash[language2][:style] << style
+        else
+          hash[language2] = {:type => value, :style => [] << style} #create a new key, with a hash of :type and :style as keys.
+        end
+      end
+    end
+  end
+  hash #return hash
 end
