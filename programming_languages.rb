d9d2aefb8561d3e60a835326b4805a8af05245ca
brittonb3 <brittonbaumann@gmail.com>
2016-02-19 09:58:04 -0600

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  new_hash[:ruby] = {:type => "interpreted", :style => [:oo]}
+  new_hash[:javascript] = {:type => "interpreted", :style => [:oo, :functional]}
+  new_hash[:python] = {:type => "interpreted", :style => [:oo]}
+  new_hash[:java] = {:type => "compiled", :style => [:oo]}
+  new_hash[:clojure] = {:type => "compiled", :style => [:functional]}
+  new_hash[:erlang] = {:type => "compiled", :style => [:functional]}
+  new_hash[:scala] = {:type => "compiled", :style => [:functional]}
+  new_hash
 end
