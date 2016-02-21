1df59cfe60d8bbcc867e7873d1fa0afdda46cf4b
jd2rogers2 <jd2rogers2@gmail.com>
2016-02-21 09:21:45 -0500

    donzo

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |a, b|
+    b.each do |c, d|
+      new_hash[c] = d
+    end
+  end
+  languages.each do |e, f|
+    f.each do |g, h|
+      new_hash[g][:style] = [e]
+    end
+  end
+  new_hash[:javascript][:style] << :oo
+  new_hash
 end
