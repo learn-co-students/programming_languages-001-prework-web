79182373e6336f96be4d69c9cbb73de258e80ed1
samuelstuckey <sam.n.stuckey@gmail.com>
2016-02-23 15:17:20 -0500

    done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  formated = {}
+  
+  languages.each_key do |sty|
+    languages[sty].each do |language|
+      #binding.pry
+      if (!(formated.include?(language[0])))
+        #binding.pry
+        formated[language[0]] = {
+          type: languages[sty][language[0]][:type], 
+          style: [sty]
+        }
+      else
+        #binding.pry
+        formated[language[0]][:style] << sty 
+
+      end   
+    end
+  end
+  formated
 end
