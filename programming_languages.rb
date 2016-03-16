a3b7b557df9d3c2cc486d04e306af375d3fa4653
steve21168 <steve21168@gmail.com>
2016-03-16 13:07:35 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'byebug'
 def reformat_languages(languages)
-  # your code here
+  reformated = {}
+
+  languages.each do |style, list_of_languages|
+    list_of_languages.each do |language, type_hash|
+      type_hash.each do |header, type_data|
+        unless reformated[language].nil?
+          reformated[language][:style] << style
+        else
+          reformated[language] = { style: [style], type: type_data}
+        end
+      end
+    end
+  end
+  reformated
 end
