7e268408a844c3407f6576d4dcddf15fabc276a0
JulieMarin <jfm7391@gmail.com>
2016-03-16 20:08:15 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -32,13 +32,11 @@ languages = {
 
 def reformat_languages(languages)
   new_hash = Hash.new(0)
-  arr =[]
   languages.each do |style, language_list|
     language_list.each do |language, data|
-      if arr.include?(language) == false
+      if new_hash.include?(language) == false
         new_hash[language] = data
         new_hash[language][:style] = [style]
-        arr << language
       else
         new_hash[language][:style] << style
       end
@@ -46,3 +44,5 @@ def reformat_languages(languages)
   end
   new_hash
 end
+
+p reformat_languages(languages)
