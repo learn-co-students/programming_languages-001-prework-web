2e92f83b021cf6a7fb5bfc4565557f3f4735a363
wengong <wentoshare@gmail.com>
2016-03-19 01:58:01 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
-  # your code here
+  xyz={}
+  languages.each do |i, g|
+    g.each do |x,y|
+      y.each do |z,k|
+        if xyz.has_key?(x)
+          xyz[x][:style][xyz[x][:style].size]=i
+        else
+          xyz[x]={z => k, :style => [i]}
+      #puts k
+        end
+      end
+    end
+  end
+  return xyz
 end
