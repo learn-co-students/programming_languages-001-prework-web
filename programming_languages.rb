25f090a79bdffc07eb3c628a8bc36c90d85d2431
nghuman <navkiran.ghuman@gmail.com>
2016-03-21 05:24:12 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |key, value|
+    value.each do |lang, v|
+      if new_hash[lang].nil?
+        new_hash[lang] = {}
+      end
+      if new_hash[lang][:style].nil?
+        new_hash[lang][:style] = []
+      end
+      new_hash[lang][:style] << key
+
+      v.each do |type, v2|
+        new_hash[lang][type] = v2
+      end
+    end
+  end
+  new_hash
 end
