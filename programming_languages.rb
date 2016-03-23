aa49345372c0a18647e852c19929d7726154b61c
alanasim <alana.simoncelli@gmail.com>
2016-03-22 22:08:05 -0400

    Done

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |styles, names|
+    names.each do |names, types|
+      new_hash[names] = {:type => languages[styles][names][:type], :style => []}
+    end
+  end
+  languages.each do |styles, names|
+    names.each do |names, types|
+      new_hash[names][:style] << styles
+    end
+  end
+  new_hash
 end
+
+reformat_languages({
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+})
