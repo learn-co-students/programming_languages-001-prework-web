803414d8c346326dfc0743543dd7b953a587269c
miggyteez <mjtavarez@yahoo.com>
2016-03-24 15:46:20 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,23 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  i = 0
+
+  languages.each do |styles, languages_collection|
+    styles_array = languages.keys
+    
+    languages_collection.each do |language, attributes|
+      language_values = attributes
+      language_values["style".to_sym] = []
+      len_styles_array = 0..(styles_array.length-1)
+
+      for i in len_styles_array
+        if languages[styles_array[i]].key?(language)
+          language_values["style".to_sym] << styles_array[i]
+        end
+      end
+
+      new_hash[language] = language_values
+    end
+  end
+  new_hash
 end
