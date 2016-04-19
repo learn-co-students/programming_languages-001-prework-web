90d2db1c2950518016bda4203cafce9eca5b2cab
Wendalf <guangw.xu@gmail.com>
2016-04-19 18:45:55 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
 def reformat_languages(languages)
   # your code here
+  reformated_languages = {}
+  languages.each do |style, language|
+    language.each do |key, value|
+      if reformated_languages.has_key?(key)
+        reformated_languages[key][:style] << style
+      else
+        reformated_languages[key] = value
+        value[:style] = [style]
+      end
+    end
+  end
+  reformated_languages
 end
