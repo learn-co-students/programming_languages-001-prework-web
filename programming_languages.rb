3f91b30afe8545066a9b5bc5316c8d8d07bca261
chugachski <natesmith.copper@gmail.com>
2016-05-11 15:37:44 -0800

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,49 @@
+require 'pry'
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+    :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    } 
+  }
+}
+
 def reformat_languages(languages)
-  # your code here
+  form_hash = {}
+  
+  languages.each do |design, data|
+    data.each do |lang, lang_data|
+      if form_hash.has_key?(lang)
+      # if the language exists as a key, 
+        form_hash[lang][:style] << design
+      else
+        form_hash[lang] = {:type => lang_data[:type], :style => [design]}
+        # if the language doesn't exist as a key (js), add another style
+      end
+    end
+  end
+  form_hash
 end
