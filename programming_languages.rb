664fd4a4fd83f48ef5d46b672a74ce5f3ffa000b
pkolodgy <pkolodgy@gmail.com>
2016-06-14 10:35:54 -0400

    finished reformat_languages method in programming_languages.rb

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |style,language|
+    language.each do |name,type_hash|
+      style_array = []
+      languages.keys.each do |k|
+        new_hash[name] = {type: type_hash[:type],
+                          style: languages[k].has_key?(name) ? style_array << k : style_array}
+      end
+    end
+  end
+  new_hash
 end
