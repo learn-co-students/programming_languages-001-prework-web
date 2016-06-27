22a25e4921f38c98025393c604287417571e57dc
gnwankwo <gnwankwo03@gmail.com>
2016-06-27 16:40:18 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash={}
+  languages. each do |weird, stuff|
+    stuff.each do |lang,info|
+      info.each do |key, value|
+    new_hash[lang]={key => value, :style =>[]}
+    end
+   end 
+  end 
+  
+  languages.each do |weird, stuff|
+  stuff.each do |lang,info|
+    if weird == languages.keys[0]
+      new_hash[lang][:style]<<weird
+    elsif weird == languages.keys[1]
+      new_hash[lang][:style]<<weird
+     end
+   end
+ end
+  return new_hash
 end
