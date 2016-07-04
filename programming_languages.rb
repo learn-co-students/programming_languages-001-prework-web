41b262e46ecf94bcd59f4f6d353a192680762137
lshapz <likeaduck@gmail.com>
2016-07-04 13:50:59 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,74 @@
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
 def reformat_languages(languages)
-  # your code here
+
+langHash = {}
+
+  languages.each do |styleLang, langs|
+    langs.each do |lang, typeHash|
+      typeValue = typeHash[:type]
+      langData = langHash[lang]
+      if langData == nil
+        langData = {}
+        langData[:type] = typeValue
+        langData[:style] = [styleLang]
+        langHash[lang] = langData
+      else
+        langData[:style] << styleLang
+        langHash[lang] = langData
+      end
+    end
+  end
+  langHash
+end
+
+def reformat_languages(languages)
+langHash = {}
+languages.each do |styleLang, langs|
+  langs.each do |lang, typeHash|
+    typeValue = typeHash[:type]
+    langData = langHash[lang]
+    if langData == nil
+      langData = {}
+      langData[:type] = typeValue
+      langData[:style] = [styleLang]
+      langHash[lang] = langData
+  else 
+    langData[:style] << styleLang
+    langHash[lang] = langData
+  end
+end
+end
+langHash
+
 end
