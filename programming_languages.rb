7b9058ef6ec11d1dfbfe2085f507de6d2994b050
Sarah Keller <sebethkeller@gmail.com>
2016-08-09 14:36:50 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
 def reformat_languages(languages)
-  # your code here
+  lang = Hash.new(0)
+  languages.each do |style, name|
+    name.each do |name, type|
+      type.each do |type, ans|
+        if lang[name] == 0
+          lang[name]= {
+            type: ans,
+            style: [style]
+          }
+        else
+          lang[name][:style] << style
+        end
+      end
+    end
+  end
+  lang
 end
