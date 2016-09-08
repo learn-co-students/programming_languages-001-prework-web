eb7a8bacd637c53f67f242c144ac1a23e9106a5c
Marc Immediato <marc@beldgroup.com>
2016-09-08 16:57:24 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  return_hash = {}
+  languages.each do |lang_type, langs_hash|
+    langs_hash.each do |lang, type_hash|
+      type_hash.each do |type_symbol,type_of_lang|
+        if return_hash[lang] == nil
+          return_hash[lang] = { type: type_of_lang, style: [lang_type] }
+        else
+          return_hash[lang][:style] << lang_type
+        end
+      end
+    end
+  end
+  return_hash
 end
